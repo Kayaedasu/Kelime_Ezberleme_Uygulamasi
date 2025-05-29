e8550fafaf5aef76f6d95de1af167d2fa4fa4ec5
HalilOguzCetin <cetinhaliloguz@gmail.com>
2025-05-29 14:16:32 +0300

    Update FormÖğrenciPanel.Designer.cs
    öğrenci panel designer

--- GirisKayit1_story/FormÖğrenciPanel.Designer.cs
+++ GirisKayit1_story/FormÖğrenciPanel.Designer.cs
@@ -39,6 +39,8 @@
             label4 = new Label();
             button5 = new Button();
             label5 = new Label();
+            btnTarih = new Button();
+            label6 = new Label();
             SuspendLayout();
             // 
             // button1
@@ -46,9 +48,9 @@
             button1.BackColor = Color.FromArgb(255, 192, 128);
             button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
             button1.BackgroundImageLayout = ImageLayout.Stretch;
-            button1.Location = new Point(57, 26);
+            button1.Location = new Point(562, 263);
             button1.Name = "button1";
-            button1.Size = new Size(172, 182);
+            button1.Size = new Size(159, 182);
             button1.TabIndex = 0;
             button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
@@ -58,7 +60,7 @@
             button2.BackColor = Color.FromArgb(255, 192, 128);
             button2.BackgroundImage = (Image)resources.GetObject("button2.BackgroundImage");
             button2.BackgroundImageLayout = ImageLayout.Stretch;
-            button2.Location = new Point(296, 36);
+            button2.Location = new Point(55, 28);
             button2.Name = "button2";
             button2.Size = new Size(163, 182);
             button2.TabIndex = 1;
@@ -68,18 +70,18 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(103, 227);
+            label1.Location = new Point(620, 462);
             label1.Name = "label1";
-            label1.Size = new Size(79, 28);
+            label1.Size = new Size(65, 23);
             label1.TabIndex = 2;
             label1.Text = "Wordle";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(316, 241);
+            label2.Location = new Point(80, 213);
             label2.Name = "label2";
-            label2.Size = new Size(133, 28);
+            label2.Size = new Size(108, 23);
             label2.TabIndex = 3;
             label2.Text = "Quiz Ayarları";
             label2.Click += label2_Click;
@@ -89,7 +91,7 @@
             button3.BackColor = Color.FromArgb(255, 192, 128);
             button3.BackgroundImage = (Image)resources.GetObject("button3.BackgroundImage");
             button3.BackgroundImageLayout = ImageLayout.Stretch;
-            button3.Location = new Point(536, 36);
+            button3.Location = new Point(296, 28);
             button3.Name = "button3";
             button3.Size = new Size(163, 182);
             button3.TabIndex = 4;
@@ -99,9 +101,9 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(594, 241);
+            label3.Location = new Point(354, 213);
             label3.Name = "label3";
-            label3.Size = new Size(56, 28);
+            label3.Size = new Size(46, 23);
             label3.TabIndex = 5;
             label3.Text = "Quiz";
             // 
@@ -110,9 +112,9 @@
             button4.BackColor = Color.FromArgb(255, 192, 128);
             button4.BackgroundImage = (Image)resources.GetObject("button4.BackgroundImage");
             button4.BackgroundImageLayout = ImageLayout.Stretch;
-            button4.Location = new Point(191, 309);
+            button4.Location = new Point(303, 263);
             button4.Name = "button4";
-            button4.Size = new Size(156, 152);
+            button4.Size = new Size(156, 182);
             button4.TabIndex = 6;
             button4.UseVisualStyleBackColor = false;
             button4.Click += button4_Click;
@@ -120,9 +122,9 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(242, 482);
+            label4.Location = new Point(354, 462);
             label4.Name = "label4";
-            label4.Size = new Size(55, 28);
+            label4.Size = new Size(46, 23);
             label4.TabIndex = 7;
             label4.Text = "LLM";
             // 
@@ -131,9 +133,9 @@
             button5.BackColor = Color.FromArgb(255, 192, 128);
             button5.BackgroundImage = (Image)resources.GetObject("button5.BackgroundImage");
             button5.BackgroundImageLayout = ImageLayout.Stretch;
-            button5.Location = new Point(436, 309);
+            button5.Location = new Point(562, 28);
             button5.Name = "button5";
-            button5.Size = new Size(159, 152);
+            button5.Size = new Size(159, 182);
             button5.TabIndex = 8;
             button5.UseVisualStyleBackColor = false;
             button5.Click += button5_Click;
@@ -141,18 +143,41 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(485, 482);
+            label5.Location = new Point(620, 213);
             label5.Name = "label5";
-            label5.Size = new Size(70, 28);
+            label5.Size = new Size(57, 23);
             label5.TabIndex = 9;
             label5.Text = "Analiz";
             // 
+            // btnTarih
+            // 
+            btnTarih.BackColor = Color.FromArgb(255, 192, 128);
+            btnTarih.BackgroundImage = (Image)resources.GetObject("btnTarih.BackgroundImage");
+            btnTarih.BackgroundImageLayout = ImageLayout.Stretch;
+            btnTarih.Location = new Point(55, 263);
+            btnTarih.Name = "btnTarih";
+            btnTarih.Size = new Size(174, 182);
+            btnTarih.TabIndex = 10;
+            btnTarih.UseVisualStyleBackColor = false;
+            btnTarih.Click += btnTarih_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(83, 462);
+            label6.Name = "label6";
+            label6.Size = new Size(105, 23);
+            label6.TabIndex = 11;
+            label6.Text = "Tarih Göster";
+            // 
             // FormÖğrenciPanel
             // 
-            AutoScaleDimensions = new SizeF(12F, 28F);
+            AutoScaleDimensions = new SizeF(10F, 22F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(255, 192, 128);
             ClientSize = new Size(779, 550);
+            Controls.Add(label6);
+            Controls.Add(btnTarih);
             Controls.Add(label5);
             Controls.Add(button5);
             Controls.Add(label4);
@@ -183,5 +208,7 @@
         private Label label4;
         private Button button5;
         private Label label5;
+        private Button btnTarih;
+        private Label label6;
     }
 }
